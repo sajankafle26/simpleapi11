7258e7553551fdaccb2ebbe0cb54a7cfae406e18
sajankafle26 <sajankafle9841@gmail.com>
2026-02-02 11:59:20 +0545

    mmm

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 // env variables
 require("dotenv").config();
 
 // Use the PORT Render provides or default to 3000
 const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
 // Root endpoint
 app.get("/", (req, res) => {
@@ -16,6 +18,34 @@ app.get("/hello/:name", (req, res) => {
   const { name } = req.params;
   res.json({ message: `Hello, ${name}!` });
 });
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+// Get all users
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+// Create new user
+app.post("/users", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
 
 // Start server
 app.listen(PORT, () => {
